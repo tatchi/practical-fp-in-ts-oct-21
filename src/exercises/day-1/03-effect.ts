df4d0c63734ded432c6d7db50450b901f7ebf8c3
tatchi <corentin.leruth@gmail.com>
2021-10-27 18:28:18 +0200

    effect

--- src/exercises/day-1/03-effect.ts
+++ src/exercises/day-1/03-effect.ts
@@ -1,4 +1,8 @@
 import * as T from "@effect-ts/core/Effect"
+import { Tagged } from "@effect-ts/system/Case"
+import { pipe } from "@effect-ts/system/Function"
+import { pretty } from "@effect-ts/core/Effect/Cause"
+import fetch, { Response, RequestInfo, RequestInit } from "node-fetch"
 
 /**
  * Theory:
@@ -45,7 +49,10 @@ export const read = T.access((_: { input: string }) => _.input)
  *
  * Test the output of the following program
  */
-export const oneLazy = T.succeedWith(() => 1)
+export const oneLazy = T.succeedWith(() => {
+  throw new Error("here")
+  return 1
+})
 
 /**
  * Exercise:
@@ -74,11 +81,26 @@ export const unit = T.unit
  * Try and test the method T.map
  */
 
+export const useMap = pipe(
+  T.succeed(1),
+  T.map((res) => `got: ${res}`)
+)
+
 /**
  * Exercise:
  *
  * Try and test the method T.chain
  */
+
+export const useChain = pipe(
+  T.succeed(1),
+  T.chain((res) => T.succeed(`got: ${res}`)),
+  T.chain((res) =>
+    T.accessM((logger: { log: (_: string) => T.UIO<void> }) =>
+      logger.log(`again got: ${res}`)
+    )
+  )
+)
 
 /**
  * Exercise:
@@ -88,11 +110,42 @@ export const unit = T.unit
  * case the number is < 0.5 and succeeds with the number otherwise
  */
 
+export class InvalidNumber extends Tagged("InvalidNumber")<{
+  readonly invalidNumber: number
+}> {}
+
+// const myRandomNumber = pipe(
+//   T.succeedWith(() => Math.floor(Math.random() * 1)),
+//   T.chain((n) => {
+//     return n < 0.5 ? T.fail(new InvalidNumber({ invalidNumber: n })) : T.succeed(n)
+//   })
+// )
+
+interface RandomNumberGeneratorService {
+  random: T.UIO<number>
+}
+
+export const generateRandomNumber = pipe(
+  T.accessM((_: RandomNumberGeneratorService) => _.random),
+  T.chain((n) => {
+    return n < 0.5 ? T.fail(new InvalidNumber({ invalidNumber: n })) : T.succeed(n)
+  })
+)
+
 /**
  * Exercise:
  *
  * Try and test the method T.tap, improve the program above to use T.tap
  */
+
+export const generateRandomNumberWithTap = pipe(
+  T.accessM((_: RandomNumberGeneratorService) => _.random),
+  T.tap((n) =>
+    pipe(
+      T.when(() => n < 0.5)(T.failWith(() => new InvalidNumber({ invalidNumber: n })))
+    )
+  )
+)
 
 /**
  * Exercise:
@@ -108,6 +161,31 @@ export const unit = T.unit
  * Handle the InvalidRandom failure using T.catchAll returning 1 as success
  * in case of failures
  */
+
+// export const generateRandomNumberOrOne = pipe(
+//   generateRandomNumberWithTap,
+//   T.catchAll((_) => T.succeed(1))
+// )
+export const generateRandomNumberOrOne = pipe(
+  generateRandomNumberWithTap,
+  T.catchTag("InvalidNumber", (_) => T.succeed(1))
+)
+
+export const randomOutput = pipe(
+  generateRandomNumberWithTap,
+  T.foldM(
+    (_) => T.succeedWith(() => `I've got an invalid number: ${_.invalidNumber}`),
+    (_) => T.succeedWith(() => `I've got: ${_}`)
+  )
+)
+
+export const randomOutput2 = pipe(
+  generateRandomNumberWithTap,
+  T.foldCauseM(
+    (_) => T.succeedWith(() => `I've got a failure: ${pretty(_)}`),
+    (_) => T.succeedWith(() => `I've got: ${_}`)
+  )
+)
 
 /**
  * Exercise:
@@ -131,17 +209,49 @@ export const unit = T.unit
  * Write a program that generate 2 valid random numbers and returns the sum
  */
 
+export const randomSum = pipe(
+  generateRandomNumberWithTap,
+  T.zip(generateRandomNumberWithTap),
+  T.map(([nb1, nb2]) => nb1 + nb2)
+)
+
 /**
  * Exercise:
  *
  * Rewrite the same program using pipe(T.do, T.bind("a", () => ...), T.bind("b", () => ...), T.map)
  */
 
+export const randomSumDo = pipe(
+  T.do,
+  T.bind("nb1", () => generateRandomNumberWithTap),
+  T.bind("nb2", () => generateRandomNumberWithTap),
+  T.map(({ nb1, nb2 }) => nb1 + nb2)
+)
+
 /**
  * Exercise:
  *
  * Rewrite the same program using T.gen
  */
+
+export const randomSumGem = T.gen(function* (_) {
+  const nb1 = yield* _(generateRandomNumberWithTap)
+  const nb2 = yield* _(generateRandomNumberWithTap)
+  return nb1 + nb2
+})
+
+export const testYield = T.gen(function* (_) {
+  const nb1 = yield* _(generateRandomNumberWithTap)
+  const nb2 = yield* _(generateRandomNumberWithTap)
+  while ((yield* _(generateRandomNumberWithTap)) > 0) {
+    yield* _(
+      T.succeedWith(() => {
+        console.log("coucou")
+      })
+    )
+  }
+  return nb1 + nb2
+})
 
 /**
  * Exercise:
@@ -157,6 +267,78 @@ export const unit = T.unit
  * 7) T.delay
  * 8) T.sleep
  */
+
+export class FetchException extends Tagged("FetchException")<{
+  readonly error: unknown
+}> {}
+
+export class MalformedJsonResponse extends Tagged("MalformedJsonResponse")<{
+  readonly error: unknown
+}> {}
+
+export const fetchRequest = T.tryCatchPromise(
+  () => fetch("https://jsonplaceholder.typicode.com/todos/1"),
+  (error) => new FetchException({ error })
+)
+
+export const fetchRequest2 = T.effectAsync<unknown, FetchException, Response>(
+  (resume) => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        resume(T.succeed(response))
+      })
+      .catch((error) => {
+        resume(T.fail(new FetchException({ error })))
+      })
+  }
+)
+
+export const fetchRequest3 = T.effectAsyncInterrupt<unknown, never, void>((resume) => {
+  console.log("STARTED")
+
+  const timer = setTimeout(() => {
+    resume(T.unit)
+    console.log("HERE")
+  }, 100)
+
+  return T.succeedWith(() => {
+    clearTimeout(timer)
+    console.log("INTERRUPTED")
+  })
+})
+
+export function _fetch(input: RequestInfo, init?: RequestInit) {
+  return T.effectAsyncInterrupt<unknown, FetchException, Response>((resume) => {
+    const controller = new AbortController()
+
+    fetch(input, { ...init, signal: controller.signal })
+      .then((response) => {
+        resume(T.succeed(response))
+      })
+      .catch((error) => {
+        resume(T.fail(new FetchException({ error })))
+      })
+
+    return T.succeedWith(() => {
+      controller.abort()
+    })
+  })
+}
+
+export function _fetchJson(input: RequestInfo, init?: RequestInit) {
+  return pipe(
+    T.tryCatchPromise(
+      () => fetch("https://jsonplaceholder.typicode.com/todos/1"),
+      (error) => new FetchException({ error })
+    ),
+    T.chain((res) =>
+      T.tryCatchPromise(
+        (): Promise<unknown> => res.json(),
+        (error) => new MalformedJsonResponse({ error })
+      )
+    )
+  )
+}
 
 /**
  * Exercise:
--- test/day-1/effect.test.ts
+++ test/day-1/effect.test.ts
@@ -36,4 +36,97 @@ describe("Day-1", () => {
 
     expect(result).toEqual(Ex.succeed("my input"))
   })
+  it("test succeed throw case", async () => {
+    let result
+
+    try {
+      result = await T.runPromise(D1.oneLazy)
+    } catch (error) {
+      result = error
+    }
+    expect(result).toEqual(new Error("here"))
+  })
+  it("test succeed throw case 2", async () => {
+    const result = await T.runPromiseExit(T.untraced(D1.oneLazy))
+
+    expect(result).toEqual(Ex.die(new Error("here")))
+  })
+  it("test T.map", async () => {
+    const result = await T.runPromiseExit(D1.useMap)
+
+    expect(result).toEqual(Ex.succeed("got: 1"))
+  })
+  it("test T.chain", async () => {
+    const messages: string[] = []
+    const result = await pipe(
+      D1.useChain,
+      T.repeatN(2),
+      T.provideAll({
+        log: (s) =>
+          T.succeedWith(() => {
+            messages.push(s)
+          })
+      }),
+      T.runPromiseExit
+    )
+    expect(result).toEqual(Ex.unit)
+    expect(messages).toEqual([
+      "again got: got: 1",
+      "again got: got: 1",
+      "again got: got: 1"
+    ])
+  })
+  it("test random number", async () => {
+    const shouldFail = await pipe(
+      D1.generateRandomNumber,
+      T.provideAll({ random: T.succeed(0.3) }),
+      // T.provideAll({ random: T.succeed(Math.floor(Math.random() * 1)) }),
+      T.runPromiseExit
+    )
+
+    // const r = pipe(
+    //   shouldFail,
+    //   Ex.mapBoth((e) => e.invalidNumber, identity)
+    // )
+
+    expect(Ex.untraced(shouldFail)).toEqual(
+      Ex.fail(new D1.InvalidNumber({ invalidNumber: 0.3 }))
+    )
+
+    const shouldSucceed = await pipe(
+      D1.generateRandomNumber,
+      T.provideAll({ random: T.succeed(0.5) }),
+      T.runPromiseExit
+    )
+
+    expect(Ex.untraced(shouldSucceed)).toEqual(Ex.succeed(0.5))
+  })
+  it("test random numberOrOne", async () => {
+    const shouldSucceedOne = await pipe(
+      D1.generateRandomNumberOrOne,
+      T.provideAll({ random: T.succeed(0.3) }),
+      T.runPromiseExit
+    )
+
+    expect(Ex.untraced(shouldSucceedOne)).toEqual(Ex.succeed(1))
+  })
+  it("test random sum", async () => {
+    const resultOk = await pipe(
+      D1.randomSum,
+      T.provideAll({ random: T.succeed(0.6) }),
+      T.runPromiseExit
+    )
+
+    expect(resultOk).toEqual(Ex.succeed(1.2))
+
+    const resultNotOk = await pipe(
+      D1.randomSum,
+      T.provideAll({ random: T.succeed(0.4) }),
+      T.runPromiseExit
+    )
+
+    expect(Ex.untraced(resultNotOk)).toEqual(
+      Ex.fail(new D1.InvalidNumber({ invalidNumber: 0.4 }))
+    )
+  })
 })
